1c81f27c0f775c11b56ca9f1baac98f03bbfc781
BryanBecerra <br.becerra.p@gmail.com>
2023-06-29 21:06:45 -0400

    Final 1.1

--- GroupZero/appweb/views.py
+++ GroupZero/appweb/views.py
@@ -214,7 +214,7 @@ def registro_obra(request):
         form = ObraForm(request.POST, request.FILES)
         if form.is_valid():
             nombreObra = form.cleaned_data.get('nombreObra')
-            nombreApellidoAutores = form.cleaned_data('nombreApellido_Autor')
+            nombreApellidoAutores = form.cleaned_data.get('nombreApellido_Autor')
             historia = form.cleaned_data.get('historia')
             imagenObra = form.cleaned_data.get('imagenObra')
             precio = form.cleaned_data.get('precio')
